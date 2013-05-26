bfba721738644e40b5f2811298d51544756be67c
razic <razic@viralkitty.com>
2013-05-26 12:31:58 +0700

    add basic test

--- test/state-machine.js
+++ test/state-machine.js
@@ -1,5 +1,34 @@
 var assert = require('assert'),
     StateMachine = require('state-machine');
 
-describe('new StateMachine(prototype)', function() {
+describe('StateMachine', function() {
+  var stateMachine;
+
+  beforeEach(function() {
+    stateMachine = new StateMachine();
+    stateMachine.pushed = 0;
+    stateMachine.state = "on";
+    stateMachine.events = {
+      "push": [
+        { from: ["on"], to: "off"  },
+        { from: ["off"], to: "on"  }
+      ]
+    };
+    stateMachine.on("on", function() {
+      this.transition(function() {
+        this.pushed += 1;
+      });
+    });
+    stateMachine.on("off", function() {
+      this.transition(function() {
+        this.pushed += 1;
+      });
+    });
+  });
+
+  describe("#transition", function() {
+    it("should be in the initial state", function() {
+      assert(stateMachine.state === "off");
+    });
+  });
 });
